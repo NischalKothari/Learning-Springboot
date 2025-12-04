b906e9cbd35dde093f30188f43ffb1553b7540e1
NischalKothari <115727839+NischalKothari@users.noreply.github.com>
2025-12-04 15:35:07 +0530

    Transaction and properties

--- 02_MongoDB Integration/mongoDbIntegration/src/main/java/com/LearningSpringBoot/mongoDbIntegration/MongoDbIntegrationApplication.java
+++ 02_MongoDB Integration/mongoDbIntegration/src/main/java/com/LearningSpringBoot/mongoDbIntegration/MongoDbIntegrationApplication.java
@@ -2,12 +2,22 @@ package com.LearningSpringBoot.mongoDbIntegration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class MongoDbIntegrationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MongoDbIntegrationApplication.class, args);
 	}
 
+    @Bean
+    public PlatformTransactionManager add(MongoDatabaseFactory dbfactory){
+        return new MongoTransactionManager(dbfactory);
+    }
 }
